f62221f565ddc295e68b90d62ab119fe223252a1
VonVolion <99684831+VonVolion@users.noreply.github.com>
2023-03-01 19:26:13 -0500

    Create optional_version_discord.js

---
+++ optional_version_discord.js
@@ -0,0 +1,48 @@
+// THIS ALTERNATE SCRIPT IS MEANT TO BE AUTOMATED WITH `crontab -e`; if you want one to be used with node, refer to the last one.
+
+const winston = require('winston');
+const logger = winston.createLogger({
+  transports: [new winston.transports.File({ filename: 'crontab.log' })],
+});
+logger.info('Crontab task started');
+// --------------------------
+//#region The modular parts of the code
+// --------------------------
+const theChannel = '';
+const myAuthorization =
+  '';
+//#endregion The modular parts of the code
+
+function sendMessageToChannel(message) {
+  const url = `https://discord.com/api/v9/channels/${theChannel}/messages`;
+  const data = {
+    content: message,
+  };
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: myAuthorization,
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+}
+
+// --------------------------
+//#region The commands you will be sending to the server
+// --------------------------
+const first_Message = '';
+const second_Message = '';
+//#endregion The commands you will be sending to the server
+
+console.log('Hello from crontab task at Discord.js !');
+
+setTimeout(() => sendMessageToChannel(first_Message), 1);
+
+setTimeout(() => sendMessageToChannel(second_Message), 523);
+
+logger.info('Crontab task completed');
