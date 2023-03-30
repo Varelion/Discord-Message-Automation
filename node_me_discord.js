a5272175400f778409dcad5facef24396e6c6cbc
Vorelion <Business.8508@gmail.com>
2023-03-30 17:58:52 -0400

    Update nodemailer setup

--- node_me_discord.js
+++ node_me_discord.js
@@ -6,13 +6,35 @@
 // Under General Headers you'll see 'Request URL',  match the number missing from const url = `https://discord.com/api/v9/channels/${theChannel}/messages`;
 // once you've entered the data into 'the channel, scroll down to 'REQUEST HEADERS', and 'authorization' will be what you put into the 'const myAuthorization' variable.
 const nodemailer = require('nodemailer');
-const theChannel = '907451784656736346';
+const theChannel = 'channel ID HERE ';
 const myAuthorization =
-  'MzYzMjcxNDU0MjM1ODIwMDMz.GaOOH9.qdCTf5WroTLOyCsNRS3Xpy0TENYnZHhP4STnk0';
-let minutes = 163;
-let timesRan = 1;
+  'AUTHORIZATION HERE ';
+let minutes = 499;
+let timesRan = 0;
 let timeExecuted = new Date('March 19 2023 ');
+let dataData;
+let errorData;
 //#endregion The modular parts of the code
+
+const consoleLogAndString = (json) => {
+  console.log('Inside consoleLogAndString', json);
+  switch (json) {
+    case (json.content = '!job performance'):
+      dataData = JSON.stringify(json);
+      errorData = 'not this time';
+      break;
+    case (json.content = '!gamble 1000'):
+      dataData = JSON.stringify(json);
+      errorData = 'not this time';
+      break;
+
+    default:
+      console.log('Hit switch case default');
+      dataData = 'not this time';
+      errorData = JSON.stringify(json);
+      break;
+  }
+};
 
 function sendMessageToChannel(message) {
   const url = `https://discord.com/api/v9/channels/${theChannel}/messages`;
@@ -29,55 +51,46 @@ function sendMessageToChannel(message) {
     body: JSON.stringify(data),
   })
     .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error(error));
+    .then((data) => consoleLogAndString(data))
+    .catch((error) => consoleLogAndString(data));
 
-  let jsonData = JSON.stringify(data);
-  let errorData = 'None this time';
-  if (error) {
-    let errorData = JSON.stringify(error);
-  }
-  if (!jsonData.content) {
-    // sender and recipient email addresses
+  // sender and recipient email addresses
 
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'business.8508@gmail.com',
-        pass: 'nhotuuabnopufdyd',
-      },
-    });
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'YOUR EMAIL HERE @gmail.com',
+      pass: 'YOUR APP PASSWORD HERE',
+    },
+  });
 
-    const myEmail = 'business.8508@gmail.com';
-    const recipientEmails = ['ighormisc@gmail.com', 'business.8508@gmail.com'];
-    let senderEmail = myEmail;
-    // message content and subject line
-    const subjectLine = `Gamble Bot: ${senderEmail}`;
+  const myEmail = 'YOUR EMAIL HERE';
+  const recipientEmails = ['TARGET@gmail.com', 'TARGET2@gmail.com'];
+  let senderEmail = myEmail;
+  // message content and subject line
+  const subjectLine = `Gamble Bot: ${senderEmail}`;
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-      from: myEmail,
-      to: recipientEmails.join(','),
-      subject: subjectLine,
-      text: message,
-      html: `<h2>DISCORD BOT FAILED</h2>
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: myEmail,
+    to: recipientEmails.join(','),
+    subject: subjectLine,
+    text: message,
+    html: `<h2>DISCORD BOT FAILED</h2>
           <br>
-          <p>jsonData: ${jsonData} </p>
+          <p>jsonData: ${dataData} </p>
                     <br>
-          <p>jsonData.content: ${jsonData.content} </p>
-                            <br>
           <p>errorData: ${errorData} </p>`,
-    };
+  };
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error, 'sending mail');
-      } else {
-        console.log(`Email sent: ${info.response}`);
-      }
-    });
-  }
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error, 'sending mail');
+    } else {
+      console.log(`Email sent: ${info.response}`);
+    }
+  });
 }
 
 // --------------------------
@@ -92,9 +105,10 @@ const func = () => {
     minutes++;
     let nowOClock = new Date();
     console.log(
-      `Minutes: ${minutes}, now is: ${nowOClock}
-      \n Times ran: ${timesRan}
-      \n Last ran at: ${timeExecuted}`
+      ` \n\n Now is: ${nowOClock},
+        \n Minutes: ${minutes},
+        \n Times ran: ${timesRan},
+        \n Last ran at: ${timeExecuted}\n`
     );
     // the thing happens
     if (minutes >= 481) {
@@ -112,24 +126,3 @@ const func = () => {
   }, 60000);
 };
 func();
-// const nextTime = () => {};
-
-// let func = async () => {
-//   while (true) {
-//     const now = new Date();
-//     const nowHour = now.getHours();
-//     const nowMinute = now.getMinutes();
-
-//     if (true) {
-//       console.log('it is time!');
-//       nextTime();
-//       // return; // exit the loop when the target time is reached
-//     }
-//     console.log('not yet');
-//     // Wait for 1 second before checking again
-//     // to avoid excessive CPU usage
-//     await new Promise((resolve) => setTimeout(resolve, 1000));
-//   }
-// };
-
-// func();
