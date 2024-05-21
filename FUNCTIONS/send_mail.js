f4fcf8ea390091709666e17aefcfa27733186a26
Varelion <99684831+Varelion@users.noreply.github.com>
2024-05-21 13:31:10 -0400

    Moule export send_mail.js

--- FUNCTIONS/send_mail.js
+++ FUNCTIONS/send_mail.js
@@ -1,42 +1,38 @@
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
+
 const { MY_EMAIL, MY_EMAIL_APP_PASSWORD, SEND_EMAIL_TO, SUBJECT_LINE, EMAIL_BODY } = process.env;
 
-// message content and subject line
-// console.log(`***
-// ***
-// ***
-// Logging:app pass
-// ***
-// ***
-// ***
-// `, SEND_EMAIL_TO.split(','));
-let transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth: {
-		user: `${MY_EMAIL}`,
-		pass: `${MY_EMAIL_APP_PASSWORD}`,
-	},
-});
+const sendEmail = async (jsonData, errorData) => {
+	// message content and subject line
+	let transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: `${MY_EMAIL}`,
+			pass: `${MY_EMAIL_APP_PASSWORD}`,
+		},
+	});
 
-// setup email data with unicode symbols
-let mailOptions = {
-	from: `${MY_EMAIL}`,
-	to: SEND_EMAIL_TO,
-	subject: `${SUBJECT_LINE}`,
-	text: EMAIL_BODY,
-	html: `<h2>DISCORD BOT FAILED</h2>
-          <br>
-          <p>jsonData:  </p>
-                    <br>
-          <p>errorData: </p>`,
+	// setup email data with unicode symbols
+	let mailOptions = {
+		from: `${MY_EMAIL}`,
+		to: SEND_EMAIL_TO,
+		subject: `${SUBJECT_LINE}`,
+		text: EMAIL_BODY,
+		html: `<h2>DISCORD BOT FAILED</h2>
+           <br>
+           <p>jsonData: ${jsonData}</p>
+           <br>
+           <p>errorData: ${errorData}</p>`,
+	};
+
+	// send mail with defined transport object
+	try {
+		const info = await transporter.sendMail(mailOptions);
+		console.log(`Email sent: ${info.response}`);
+	} catch (error) {
+		console.log(error, 'sending mail');
+	}
 };
 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, (error, info) => {
-	if (error) {
-		console.log(error, 'sending mail');
-	} else {
-		console.log(`Email sent: ${info.response}`);
-	}
-});
+module.exports = sendEmail;
---
+++ index.js
@@ -0,0 +1,4 @@
+const sendEmail = require('./FUNCTIONS/send_mail');
+
+// Call the sendEmail function with your data
+sendEmail('Your JSON data', 'Your error data');
